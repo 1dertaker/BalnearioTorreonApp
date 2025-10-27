7f420becde440a0bf4a2d88f565527fe51eb9860
GabrielMoron <morongabriel66@gmail.com>
2025-10-26 23:18:28 -0300

    Update Cliente.java

---
+++ src/main/java/classes/Cliente.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String contacto;
+
+    public Cliente(int id, String nombre, String contacto) {
+        this.id = id; this.nombre = nombre; this.contacto = contacto;
+    }
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
+    public String getContacto() { return contacto; }
+}
