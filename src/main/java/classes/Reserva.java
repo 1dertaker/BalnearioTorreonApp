ffa0bd934fe6f8ec5533fc2bd2bd299f74a59163
GabrielMoron <morongabriel66@gmail.com>
2025-10-29 21:32:00 -0300

    Update Reserva.java

--- src/main/java/classes/Reserva.java
+++ src/main/java/classes/Reserva.java
@@ -1,36 +1,51 @@
 package classes;
 
-
-import java.time.LocalDate;
-import java.util.Set;
-
 public class Reserva {
     private int id;
-    private int carpaId;
     private int clienteId;
-    private LocalDate desde;
-    private LocalDate hasta;
-    private TipoAlquiler tipoAlquiler;
-    private Set<ServicioExtra> extras;
-    private double precioFinal;
+    private int carpaId;
+    private String fechaDesde;
+    private String fechaHasta;
+    private String tipo;
+    private boolean activa;
 
-    public Reserva(int id, int carpaId, int clienteId, LocalDate desde, LocalDate hasta, TipoAlquiler tipoAlquiler, Set<ServicioExtra> extras) {
-        this.id = id; this.carpaId = carpaId; this.clienteId = clienteId; this.desde = desde; this.hasta = hasta; this.tipoAlquiler = tipoAlquiler; this.extras = extras;
+    public Reserva() {}
+
+    public Reserva(int clienteId, int carpaId, String fechaDesde, String fechaHasta, String tipo) {
+        this.clienteId = clienteId;
+        this.carpaId = carpaId;
+        this.fechaDesde = fechaDesde;
+        this.fechaHasta = fechaHasta;
+        this.tipo = tipo;
+        this.activa = true;
     }
 
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getClienteId() { return clienteId; }
+    public void setClienteId(int clienteId) { this.clienteId = clienteId; }
+
     public int getCarpaId() { return carpaId; }
-    public LocalDate getDesde() { return desde; }
-    public LocalDate getHasta() { return hasta; }
-    public void setPrecioFinal(double p) { this.precioFinal = p; }
-    public double getPrecioFinal() { return precioFinal; }
-    public TipoAlquiler getTipoAlquiler() { return tipoAlquiler; }
-    public Set<ServicioExtra> getExtras() { return extras; }
+    public void setCarpaId(int carpaId) { this.carpaId = carpaId; }
+
+    public String getFechaDesde() { return fechaDesde; }
+    public void setFechaDesde(String fechaDesde) { this.fechaDesde = fechaDesde; }
+
+    public String getFechaHasta() { return fechaHasta; }
+    public void setFechaHasta(String fechaHasta) { this.fechaHasta = fechaHasta; }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public boolean isActiva() { return activa; }
+    public void setActiva(boolean activa) { this.activa = activa; }
+
+    @Override
+    public String toString() {
+        return "Reserva #" + id + " [Cliente " + clienteId + ", Carpa " + carpaId +
+                ", " + tipo + " desde " + fechaDesde + " hasta " + fechaHasta + "]";
+    }
 }
-﻿
-1dertaker
-1ndertaker
- 
- 
- 
- 
- https://github.com/1dertaker
+
+
