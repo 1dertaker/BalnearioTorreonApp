fcc32550f6f7c02c97d8c07fb51b40acd2ba7748
cirit03 <baezciro2016@gmail.com>
2025-11-10 21:35:24 -0300

    bTorreonApp

--- src/main/java/repo/RepositorioClientes.java
+++ src/main/java/repo/RepositorioClientes.java
@@ -1,48 +1,19 @@
 package repo;
 
 import classes.*;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
-import java.lang.reflect.Type;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.*;
 
-public class RepositorioClientes {
-    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private final Path filePath = Path.of(System.getProperty("user.dir"), "clientes.json");
 
+
+public class RepositorioClientes extends RepositorioGenerico<Cliente> implements IRepositorio<Cliente> {
     public RepositorioClientes() {
-        try {
-            Files.createDirectories(filePath.getParent());
-            if (!Files.exists(filePath)) Files.writeString(filePath, "[]");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        super("clientes.json", new TypeToken<List<Cliente>>() {}.getType());
     }
 
-    private List<Cliente> readAll() {
-        try (Reader reader = Files.newBufferedReader(filePath)) {
-            Type listType = new TypeToken<List<Cliente>>() {}.getType();
-            List<Cliente> clientes = gson.fromJson(reader, listType);
-            return clientes != null ? clientes : new ArrayList<>();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ArrayList<>();
-        }
-    }
-
-    private void saveAll(List<Cliente> clientes) {
-        try (Writer writer = Files.newBufferedWriter(filePath)) {
-            gson.toJson(clientes, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
+    @Override
     public Cliente agregar(Cliente c) {
         List<Cliente> clientes = readAll();
         int nextId = clientes.stream().mapToInt(Cliente::getId).max().orElse(0) + 1;
@@ -52,14 +23,17 @@ public class RepositorioClientes {
         return c;
     }
 
+    @Override
     public List<Cliente> listar() {
         return readAll();
     }
 
+    @Override
     public Optional<Cliente> buscarPorId(int id) {
         return readAll().stream().filter(c -> c.getId() == id).findFirst();
     }
 
+    @Override
     public boolean eliminar(int id) {
         List<Cliente> clientes = readAll();
         boolean removed = clientes.removeIf(c -> c.getId() == id);
